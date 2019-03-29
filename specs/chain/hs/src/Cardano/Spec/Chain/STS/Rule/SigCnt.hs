838e690b76f3b89385ff2955e601aea166b6b7d2
Marko Dimjašević <marko.dimjasevic@iohk.io>
2019-03-29 13:16:05 +0100

    Issue #364: removes the NonInjectiveDelegationMap predicate failure in the SigCnt STS

--- specs/chain/hs/src/Cardano/Spec/Chain/STS/Rule/SigCnt.hs
+++ specs/chain/hs/src/Cardano/Spec/Chain/STS/Rule/SigCnt.hs
@@ -4,6 +4,7 @@ module Cardano.Spec.Chain.STS.Rule.SigCnt where
 
 import Control.Lens ((^.), to)
 import qualified Data.Map.Strict as Map
+import Data.Maybe (listToMaybe)
 import Data.Map.Strict (Map)
 import Data.Sequence (Seq, (|>))
 import qualified Data.Sequence as S
@@ -29,12 +30,6 @@ instance STS SIGCNT where
     = TooManyIssuedBlocks VKeyGenesis -- The given genesis key issued too many blocks.
     | NotADelegate
     -- ^ The key signing the block is not a delegate of a genesis key.
-    | NonInjectiveDelegationMap
-    -- ^ Delegation rules should restrict the delegation map to be injective.
-    --
-    -- Should not be needed once
-    -- https://github.com/input-output-hk/cardano-chain/issues/257 gets
-    -- resolved.
 
     deriving (Eq, Show)
 
@@ -45,21 +40,18 @@ instance STS SIGCNT where
         TRC ((pps, dms), sgs, vk) <- judgmentContext
         let k = pps ^. stableAfter . to unBlockCount . to fromIntegral
             t = pps ^. bkSgnCntT
-        case Map.keys (Map.filter (== vk) dms) of
+        case listToMaybe (Map.keys (Map.filter (== vk) dms)) of
         -- Currently we do not restrict the number of delegators to a given key
         -- in the delegation rules. See: https://github.com/input-output-hk/cardano-chain/issues/257
         --
         -- Even we implement the restriction in the delegation rules, we might
         -- still want to check this (unless the types forbid such situation).
-          [vkG] -> do
+          Just vkG -> do
             let sgs' = S.drop (S.length sgs + 1 - k) (sgs |> vkG)
                 nrSignedBks = fromIntegral (S.length (S.filter (==vkG) sgs'))
             nrSignedBks <= fromIntegral k * t ?! TooManyIssuedBlocks vkG
             return $! sgs'
-          [] -> do
+          Nothing -> do
             failBecause NotADelegate
             return sgs -- TODO: this is a quite inconvenient encoding for this transition system!
-          _ -> do
-            failBecause NonInjectiveDelegationMap
-            return sgs
     ]
